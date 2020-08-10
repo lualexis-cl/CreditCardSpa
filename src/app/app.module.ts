87a3beca9040d342a3ef98be43492d38a05bdad7
Luis Alexis <Luis Alexis@DESKTOP-T8UDD83>
2020-08-10 15:49:47 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './_components/payment-details/payment-details.component';
+import { PaymentDetailComponent } from './_components/payment-details/payment-detail/payment-detail.component';
+import { PaymentDetailListComponent } from './_components/payment-detail-list/payment-detail-list.component';
+import { PaymentDetailService } from './_services/payment-detail.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailComponent,
+    PaymentDetailListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PaymentDetailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
